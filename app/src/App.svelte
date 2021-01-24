c80134eb023514195ebac1630832b534e84b5c2a
Martin Indico <martin@indicosys.com>
2021-01-24 15:21:04 +0100

    Updated version

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -7,5 +7,12 @@
 </main>
 
 <style>
-	
+		main {
+			width: 100%;
+			height: 100%;
+			background-color: #FFCC80;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
 </style>
